72c3f0bd2345e40d692dd01a63ba0ce760339a79
metalork <rinconrj@gmail.com>
2019-01-01 13:22:53 -0200

    primer comit

---
+++ server/server.js
@@ -0,0 +1,48 @@
+require('./config/config')
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+ app.use(bodyParser.urlencoded({extended: false}))
+
+ app.use(bodyParser.json())
+
+
+app.get('/usuario', function (req, res) {
+  res.json(bodyParser.json())
+
+});
+
+app.post('/usuario', function (req, res) {
+    let body = req.body;
+
+    if(body.nombre === undefined){
+        res.status(400).json({
+            ok: false,
+            mensaje: 'el nombre enviado no es correcto'
+        });
+    }else{
+      res.json({
+          persona: body});  
+    }
+
+
+  });
+
+
+  app.put('/usuario/:id', function (req, res) {
+    let id = req.params.id;
+  
+    res.jason({
+        id
+    });
+  });
+ 
+app.delete('/usuario', function (req, res) {
+    res.json(bodyParser.json())
+  
+  });
+app.listen(process.env.PORT,() => {
+    console.log('Escuchando puertos: ', process.env.PORT);
+});
