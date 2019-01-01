87e928b77247b85e4fc81e8d07eaf98ae9432515
metalork <rinconrj@gmail.com>
2019-01-01 14:25:34 -0200

    cambios finales

--- server/server.js
+++ server/server.js
@@ -1,48 +1,53 @@
-require('./config/config')
+require('./config/config');
 
 const express = require('express');
 const app = express();
+
 const bodyParser = require('body-parser');
 
- app.use(bodyParser.urlencoded({extended: false}))
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
 
- app.use(bodyParser.json())
+// parse application/json
+app.use(bodyParser.json())
 
 
-app.get('/usuario', function (req, res) {
-  res.json('deg usuario local')
+app.get('/usuario', function(req, res) {
+    res.json('get Usuario LOCAL!!!');
+});
+
+app.post('/usuario', function(req, res) {
+
+    let body = req.body;
+
+    if (body.nombre === undefined) {
+
+        res.status(400).json({
+            ok: false,
+            mensaje: 'El nombre es necesario'
+        });
+
+    } else {
+        res.json({
+            persona: body
+        });
+    }
 
 });
 
-app.post('/usuario', function (req, res) {
-    let body = req.body;
+app.put('/usuario/:id', function(req, res) {
 
-    if(body.nombre === undefined){
-        res.status(400).json({
-            ok: false,
-            mensaje: 'el nombre enviado no es correcto'
-        });
-    }else{
-      res.json({
-          persona: body});  
-    }
-
-
-  });
-
-app.put('/usuario/:id', function (req, res) {
-    
     let id = req.params.id;
-  
-    res.jason({
+
+    res.json({
         id
     });
-  });
- 
-app.delete('/usuario', function (req, res) {
-    res.json('delete usuario')
-  });
+});
 
-app.listen(process.env.PORT,() => {
-    console.log('Escuchando puertos: ', process.env.PORT);
+app.delete('/usuario', function(req, res) {
+    res.json('delete Usuario');
+});
+
+app.listen(process.env.PORT, () => {
+    console.log('Escuchando puerto: ', process.env.PORT);
 });
