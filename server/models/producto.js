aa458254292707d60418263b61b5a3958a8db0ad
ricardo <rinconrj@gmail.com>
2019-05-24 21:03:44 -0300

    test

--- server/models/producto.js
+++ server/models/producto.js
@@ -1,16 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const history = require("mongoose-history");
 const Schema = mongoose.Schema;
 
-
 let productoSchema = new Schema({
-    nombre: { type: String, required: [true, 'El nombre es necesario'] },
-    precioUni: { type: Number, required: [true, 'El precio únitario es necesario'] },
-    descripcion: { type: String, required: false },
-    disponible: { type: Boolean, required: true, default: true },
-    categoria: { type: Schema.Types.ObjectId, ref: 'categoria', required: true },
-    usuario: { type: Schema.Types.ObjectId, ref: 'Usuario' },
-    img: { type: String, required: false }
+  nombre: { type: String, required: [true, "El nombre es necesario"] },
+  precioUni: { type: Number, required: [true, "El precio únitario es necesario"] },
+  descripcion: { type: String, required: false },
+  disponible: { type: Boolean, required: true, default: true },
+  categoria: { type: Schema.Types.ObjectId, ref: "categoria", required: true },
+  usuario: { type: Schema.Types.ObjectId, ref: "Usuario" },
+  img: { type: String, required: false }
 });
+productoSchema.plugin(history, { customCollectionName: "myProgram" });
 
-
-module.exports = mongoose.model('Producto', productoSchema);
+module.exports = mongoose.model("Producto", productoSchema);
--- server/routes/producto.js
+++ server/routes/producto.js
@@ -1,182 +1,162 @@
-const express = require('express');
+const express = require("express");
 
-let {verificaToken, verificaAdmin_Role} =require('../middlewares/autenticacion');
+let { verificaToken, verificaAdmin_Role } = require("../middlewares/autenticacion");
 
 let app = express();
 
-let Producto = require('../models/producto');
+let Producto = require("../models/producto");
 
-const _ = require('underscore');
+const _ = require("underscore");
 
+const ProductoHistory = Producto.historyModel();
 
-
-app.get('/producto', verificaToken, (req, res)=>{
-
-    Producto.find({disponible:true})
-        .sort('nombre')
-        .populate('usuario', 'nombre email')
-        .populate('categoria', 'descripcion')
-        .exec((err, productos) =>{
-            if (err){
-                return res.status(400).json({
-                ok: false,
-                err
-            });
-        }res.json({
-            ok:true,
-            productos
-            })
-        })
-});
-
-
-app.get('/producto/:id', verificaToken, (req, res)=>{
-    
-    let id = req.params.id;
-    
-    Producto.findById(id)
-            .populate('usuario', 'nombre email')
-             .populate('categoria', 'descripcion')
-             .exec( (err, productoDB) =>{
-            if (err){
-                return res.status(400).json({
-                ok: false,
-                err
-            });
-        }res.json({
-            ok:true,
-            producto: productoDB
-            })
-        })
+app.get("/producto", verificaToken, (req, res) => {
+  Producto.find({ disponible: true })
+    .sort("nombre")
+    .populate("usuario", "nombre email")
+    .populate("categoria", "descripcion")
+    .exec((err, productos) => {
+      if (err) {
+        return res.status(400).json({
+          ok: false,
+          err
+        });
+      }
+      res.json({
+        ok: true,
+        productos
+      });
     });
-    
-
-    app.get('/producto/buscar/:termino', verificaToken, (req, res)=>{
-    
-        let termino = req.params.termino;
-        let regex = new RegExp(termino, 'i');
-        
-        Producto.find({nombre:regex})
-                 .populate('categoria', 'nombre')
-                 .exec( (err, productoDB) =>{
-                if (err){
-                    return res.status(400).json({
-                    ok: false,
-                    err
-                });
-            }res.json({
-                ok:true,
-                producto: productoDB
-                })
-            })
-        });
-
-
-
-app.post('/producto', verificaToken, (req, res)=>{
-
-        let body = req.body;
-
-        let producto = new Producto({
-            nombre: body.nombre,
-            precioUni: body.precioUni,
-            descripcion: body.descripcion,
-            disponible: body.disponible,
-            categoria: body.categoria,
-            usuario: req.usuario._id,
-            img: body.img
-            });
-
-            producto.save((err, productoDB)=>{
-                if (err) {
-                    return res.status(500).json({
-                    ok: false,
-                    err
-                });
-            }
-            if (!productoDB){
-                return res.status(400).json({
-                    ok: false,
-                    err
-                });
-            }
-            res.json({
-                ok:true,
-                producto: productoDB
-                })
-            });
-    
 });
 
-app.put('/producto/:id', verificaToken, (req, res)=>{
+app.get("/producto/:id", verificaToken, (req, res) => {
+  let id = req.params.id;
 
-    let id = req.params.id;
-    let body = req.body;
-
-    Producto.findByIdAndUpdate(id, body, {new: true, runValidators:true}, (err, productoDB)=>{
-
-        if (err){
-            return res.status(500).json({
-            ok: false,
-            err
-        });
-    }
-    if (!productoDB){
+  Producto.findById(id)
+    .populate("usuario", "nombre email")
+    .populate("categoria", "descripcion")
+    .exec((err, productoDB) => {
+      if (err) {
         return res.status(400).json({
-            ok: false,
-            err
+          ok: false,
+          err
         });
-    }
-
-
-        res.json({
-            ok:true,
-            producto: productoDB
-        }); 
-    })
-
-    // actualizar categoria
- 
-    
+      }
+      res.json({
+        ok: true,
+        producto: productoDB
+      });
+    });
 });
 
+app.get("/producto/buscar/:termino", verificaToken, (req, res) => {
+  let termino = req.params.termino;
+  let regex = new RegExp(termino, "i");
 
-app.delete('/producto/:id', verificaToken, (req, res) =>{
-   
-    
-    let id = req.params.id;
-    let body = _.pick(req.body, ['disponible']);
-
-    Producto.findByIdAndUpdate(id, {disponible: false}, {new: true}, (err, productoBorrado)=>{
-        
-        if (err){
-            return res.status(400).json({
-            ok: false,
-            err
-        });
-    };
-
-    if(!productoBorrado){
+  Producto.find({ nombre: regex })
+    .populate("categoria", "nombre")
+    .exec((err, productoDB) => {
+      if (err) {
         return res.status(400).json({
-            ok: false,
-            error: {
-                message: 'Producto no encontrado'
-            }
+          ok: false,
+          err
         });
+      }
+      res.json({
+        ok: true,
+        producto: productoDB
+      });
+    });
+});
+
+app.post("/producto", verificaToken, (req, res) => {
+  let body = req.body;
+
+  let producto = new Producto({
+    nombre: body.nombre,
+    precioUni: body.precioUni,
+    descripcion: body.descripcion,
+    disponible: body.disponible,
+    categoria: body.categoria,
+    usuario: req.usuario._id,
+    img: body.img
+  });
+
+  producto.save((err, productoDB) => {
+    if (err) {
+      return res.status(500).json({
+        ok: false,
+        err
+      });
+    }
+    if (!productoDB) {
+      return res.status(400).json({
+        ok: false,
+        err
+      });
+    }
+    ProductoHistory;
+    res.json({
+      ok: true,
+      producto: productoDB
+    });
+  });
+});
+
+app.put("/producto/:id", verificaToken, (req, res) => {
+  let id = req.params.id;
+  let body = req.body;
+
+  Producto.findByIdAndUpdate(id, body, { new: true, runValidators: true }, (err, productoDB) => {
+    if (err) {
+      return res.status(500).json({
+        ok: false,
+        err
+      });
+    }
+    if (!productoDB) {
+      return res.status(400).json({
+        ok: false,
+        err
+      });
     }
 
     res.json({
-        ok: true,
-        producto: productoBorrado,
-        mensaje: 'producto borrado'
-    })
+      ok: true,
+      producto: productoDB
+    });
+  });
 
-
-    })
-
-
-
+  // actualizar categoria
 });
 
+app.delete("/producto/:id", verificaToken, (req, res) => {
+  let id = req.params.id;
+  let body = _.pick(req.body, ["disponible"]);
+
+  Producto.findByIdAndUpdate(id, { disponible: false }, { new: true }, (err, productoBorrado) => {
+    if (err) {
+      return res.status(400).json({
+        ok: false,
+        err
+      });
+    }
+
+    if (!productoBorrado) {
+      return res.status(400).json({
+        ok: false,
+        error: {
+          message: "Producto no encontrado"
+        }
+      });
+    }
+
+    res.json({
+      ok: true,
+      producto: productoBorrado,
+      mensaje: "producto borrado"
+    });
+  });
+});
 
 module.exports = app;
